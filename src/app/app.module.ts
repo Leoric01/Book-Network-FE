0fe037f2bda91587e2a1c3579d6b7e6d9cfe045a
Leoric01 <troglodit.nick@seznam.cz>
2024-10-07 15:50:10 +0200

    fe implementing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,37 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {provideHttpClient, HttpClientModule, HTTP_INTERCEPTORS, withFetch} from '@angular/common/http';
+import { LoginComponent } from './pages/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { CodeInputModule } from 'angular-code-input';
+import { RegisterComponent } from './pages/register/register.component';
+import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
+import { HttpTokenInterceptor } from './services/interceptor/http-token.interceptor';
+// import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ActivateAccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    CodeInputModule
   ],
   providers: [
-    provideClientHydration()
+    provideHttpClient(withFetch()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpTokenInterceptor,
+      multi: true
+    },
   ],
   bootstrap: [AppComponent]
 })
