1398b27ca31ff87331f358939f7dd41d94e87a87
Leoric01 <troglodit.nick@seznam.cz>
2024-10-08 02:39:40 +0200

    rating now does show up

--- src/app/modules/book/book.module.ts
+++ src/app/modules/book/book.module.ts
@@ -7,11 +7,10 @@ import {FormsModule} from '@angular/forms';
 import {MenuComponent} from './components/menu/menu.component';
 import {BookCardComponent} from './components/book-card/book-card.component';
 import {BookListComponent} from './pages/book-list/book-list.component';
-// import { BookListComponent } from './pages/book-list/book-list.component';
+import {RatingComponent} from './components/rating/rating.component';
 // import { MyBooksComponent } from './pages/my-books/my-books.component';
 // import { ManageBookComponent } from './pages/manage-book/manage-book.component';
 // import { BorrowedBookListComponent } from './pages/borrowed-book-list/borrowed-book-list.component';
-// import { RatingComponent } from './components/rating/rating.component';
 // import { ReturnedBooksComponent } from './pages/returned-books/returned-books.component';
 // import { BookDetailsComponent } from './pages/book-details/book-details.component';
 
@@ -22,11 +21,10 @@ import {BookListComponent} from './pages/book-list/book-list.component';
     MenuComponent,
     BookCardComponent,
     BookListComponent,
-    // BookListComponent,
+    RatingComponent,
     // MyBooksComponent,
     // ManageBookComponent,
     // BorrowedBookListComponent,
-    // RatingComponent,
     // ReturnedBooksComponent,
     // BookDetailsComponent
   ],
