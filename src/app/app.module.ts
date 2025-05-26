215b4b6f5ee3e3a10413af60afbddf7297e9ad35
Leoric01 <troglodit.nick@seznam.cz>
2025-05-26 09:59:41 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,42 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withFetch} from '@angular/common/http';
-import {LoginComponent} from './pages/login/login.component';
-import {FormsModule} from '@angular/forms';
-import {CodeInputModule} from 'angular-code-input';
-import {RegisterComponent} from './pages/register/register.component';
-import {ActivateAccountComponent} from './pages/activate-account/activate-account.component';
-import {HttpTokenInterceptor} from './services/interceptor/http-token.interceptor';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
+import { LoginComponent } from './pages/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { CodeInputModule } from 'angular-code-input';
+import { RegisterComponent } from './pages/register/register.component';
+import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
+import { HttpTokenInterceptor } from './services/interceptor/http-token.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    ActivateAccountComponent
+    ActivateAccountComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    CodeInputModule
+    CodeInputModule,
   ],
   providers: [
     provideHttpClient(withFetch()),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpTokenInterceptor,
-      multi: true
+      multi: true,
     },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
